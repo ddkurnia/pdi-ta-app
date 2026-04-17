288bd350424edca35858f0dfe32840eb0921fe2a
ddkurnia <kurnia200688@gmail.com>
2026-04-17 16:24:52 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB.....",
+  apiKey: "AIzaSyBUcw8o7otTVB97wR0mMJIo2LNkS_oSB5Y",
   authDomain: "pdi-ta-app.firebaseapp.com",
   projectId: "pdi-ta-app",
-  storageBucket: "pdi-ta-app.firebasestorage.app",
+  storageBucket: "pdi-ta-app.appspot.com",
   messagingSenderId: "477558489412",
-  appId: "1:477558489412:web:..."
+  appId: "1:477558489412:web:b0898e239f2145a5299711"
 };
 
 firebase.initializeApp(firebaseConfig);
